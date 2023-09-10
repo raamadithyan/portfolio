17e4ad652a3fd3ceb233246652c5e97ef40b9f56
raamadithyan <chitha.don@gmail.com>
2023-09-11 01:42:18 +0530

    done responsive design for portfolio page

--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -9,12 +9,22 @@ export default function Portfolio() {
     { title: 'Card 2', content: 'Content for Card 2',color:'#7CCAA0',path:'two' },
     { title: 'Card 3', content: 'Content for Card 3',color:'#F4F592' ,path:'three'},
     { title: 'Card 3', content: 'Content for Card 3',color:'#3CCAA0',path:'four' },
-
   ];
   return (
     <Page>
-    <div className='  bg-[#333] w-full h-[100vh] absolute top-0 left-0 px-96 flex items-center justify-center'>
-      <div className="app z-20   ">
+    <div className='  bg-[#333] w-full h-fit absolute top-0 left-0 flex items-center justify-center
+     sm:h-full
+    '>
+      <div 
+        className="
+           
+           flex flex-wrap items-center justify-center
+          gap-12 p-12 mt-28
+          w-fit h-full  z-20
+          sm:w-full sm:h-fit sm:flex sm:mt-14 sm:mx-80
+          
+
+             ">
       {cards.map((card, index) => (
         <Card 
          key={card.title} title={card.title} content={card.content} 
