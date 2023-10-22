8b7b4394f00dd8b1d53879a2c4e6390c92dae318
raamadithyan <chitha.don@gmail.com>
2023-10-23 03:44:54 +0530

    done landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,47 @@ import Page from '@/components/Page'
 export default function Home() {
   return (
     <Page>
-    <h4 className='bg-[#5e44e1] w-full h-[100vh] absolute top-0 left-0 -z-10'>
-      {/*Hello Motion*/}
-    </h4>
+    <div 
+      className='
+        bg-[#5e44e1] w-full h-[100vh]
+         border-yellow-600 border border-1 flex justify-center items-center
+
+         '>
+         <div className=' border-yellow-600 border border-1 w-[80%] sm:w-[50%] h-[50%]'>
+      <h1 
+      className='
+        font-bold
+        text-white
+        text-2xl
+        text-center
+        sm:text-4xl
+
+        '>
+
+
+      Crafting Exquisite Digital Experiences
+
+      </h1>
+      <h1 
+      className='
+        font-normal
+        text-white
+        mt-8
+        text-md
+        sm:text-xl
+        text-center
+        '>
+              Hey there! I'm Raam Adithyan, a passionate 
+      frontend developer with a flair for creating 
+      seamless user experiences. I specialize in crafting
+       intuitive, interactive, and modern web apps that 
+       leave a lasting impression.
+       
+      </h1>
+
+           
+         </div>
+    </div>
     </Page>
   )
 }
