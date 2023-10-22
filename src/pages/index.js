cd030de651aad0ee7715671cc69695a3b9327cfe
raamadithyan <chitha.don@gmail.com>
2023-10-22 16:15:50 +0530

    removed sample text

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <Page>
     <h4 className='bg-[#5e44e1] w-full h-[100vh] absolute top-0 left-0 -z-10'>
-      Hello Motion
+      {/*Hello Motion*/}
     </h4>
     </Page>
   )
