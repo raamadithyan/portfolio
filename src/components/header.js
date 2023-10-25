916292f4fcb9bc1458851871aba85be212d5d51d
raamadithyan <chitha.don@gmail.com>
2023-10-26 02:46:47 +0530

    added resume page layout

--- src/components/header.js
+++ src/components/header.js
@@ -1,48 +1,55 @@
- import Link from "next/link";
-import { Rammetto_One  } from 'next/font/google'
+import Link from "next/link";
+import { Rammetto_One } from "next/font/google";
 
-
-
- import { createPortal } from "react-dom";
+import { createPortal } from "react-dom";
 
 const rammetto = Rammetto_One({
-  weight: '400',
-  subsets: ['latin'],
-})
- 
- const Header = () => {
- 	return (
- 		<header className=" justify-center 
- 		fixed w-full flex sm:justify-between py-4 sm:px-96 z-40">
+  weight: "400",
+  subsets: ["latin"],
+});
 
- 		<div className="text-2xl py-0 block z-50
- 		sm:flex sm:gap-12 sm:py-4  sm:text-xl">
-         <div className={rammetto.className}>
+const Header = () => {
+  return (
+    <header
+      className=" justify-center 
+    fixed w-full flex sm:justify-between py-4 sm:px-96 z-40"
+    >
+      <div
+        className="text-2xl py-0 block z-50
+    sm:flex sm:gap-12 sm:py-4  sm:text-xl"
+      >
+        <div className={rammetto.className}>
+          <Link href="/"> Raam Adithyan</Link>
+        </div>
+      </div>
 
- 		<Link href="/"> Raam Adithyan</Link>
- 		</div>
- 		</div>
+      <div
+        className=" absolute  flex py-16 gap-8
+    sm:flex sm:gap-12 sm:py-4 sm:relative
+    "
+      >
+        <div className="header-item text-xl">
+          <Link className="text-[1rem] sm:text-[1.2rem]" href="/portfolio">
+            {" "}
+            Portfolio
+          </Link>
+        </div>
 
- 		<div className=" absolute  flex py-16 gap-8
- 		sm:flex sm:gap-12 sm:py-4 sm:relative
- 		">
+        <div className="header-item text-xl">
+          <Link className="text-[1rem] sm:text-[1.2rem]" href="/resume">
+            {" "}
+            Resume
+          </Link>
+        </div>
 
- 		<div className="header-item text-xl">
- 		<Link className="text-[1rem] sm:text-[1.2rem]" href="/portfolio"> Portfolio</Link>
- 		</div>
+        <div className="header-item text-xl">
+          <Link className="text-[1rem] sm:text-[1.2rem]" href="/connect">
+            Connect
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+};
 
- 		<div className="header-item text-xl">
- 		<Link className="text-[1rem] sm:text-[1.2rem]" href="/resume"> Resume</Link>
- 		</div>
-
- 		<div  className="header-item text-xl">
- 		<Link className="text-[1rem] sm:text-[1.2rem]" href="/connect">Connect</Link>
- 		</div>
-
- 		</div>
- 			
- 		</header>
- 	)
- }
- 
- export default Header
+export default Header;
--- src/pages/connect.js
+++ src/pages/connect.js
@@ -1,15 +1,15 @@
-import Page from '@/components/Page'
+import Page from "@/components/Page";
 
 export default function Connect() {
   return (
     <Page>
-    <div className='bg-[#5cc9c9] w-full h-[100vh] absolute top-0 left-0 -z-10
+      <div
+        className="bg-[#5cc9c9] w-full h-[100vh] absolute top-0 left-0 -z-10
                         flex justify-center items-center
-    '>
-      <div className=' border-2  w-[80%] sm:w-[50%] h-auto'>
-    
-    </div>
-    </div>
+    "
+      >
+        <div className="  border-2  w-[80%] sm:w-[50%] h-auto"></div>
+      </div>
     </Page>
-  )
+  );
 }
